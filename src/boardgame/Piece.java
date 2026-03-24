5aa2f36777ca48dd8e76a33830637f9ffc357456
Joao Molina <molina.joaov@gmail.com>
2026-03-24 20:43:50 -0300

    Chess layer and printing the board

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -2,4 +2,16 @@ package boardgame;
 
 public class Piece {
 
+	protected Position position;
+	private Board board;
+	
+	
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
+
+	protected Board getBoard() { //esse tabuleiro é de uso interno da camada de tabuleiro
+		return board;
+	}
 }
